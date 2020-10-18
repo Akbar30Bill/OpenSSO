682081dcf43210652973f78209edb2f93ae9ed2b
alishahverdi <alishahverdi2010@yahoo.com>
2020-10-19 02:30:33 +0330

    added services sign up with PSK

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ app = Flask(__name__)
 @app.route('/signup/user', methods=['POST'])
 def SignUpUser():
     data = request.json
-    print(data)
     username, password = data['username'], data['password']
     result = create_user(username, password)
     return result
@@ -17,7 +16,6 @@ def SignUpUser():
 @app.route('/signup/service', methods=['POST'])
 def SignUpService():
     data = request.json
-    print(data)
     name, username, password = data['name'], data['username'], data['password']
-    result = create_user(username, password)
+    result = create_service(name, username, password)
     return result
--- db.py
+++ db.py
@@ -1,6 +1,8 @@
 import os
 from psycopg2 import connect
 import hashlib
+from random_word import RandomWords
+r = RandomWords()
 
 dbcon = connect(
     database= os.getenv('POSTGRES_DB'),
@@ -21,3 +23,19 @@ def create_user(username, password):
     })
     dbcon.commit()
     return 'safa'
+
+def create_service(name, username, password):
+    PSK =  '-'.join(r.get_random_words(limit=10))
+    cur = dbcon.cursor()
+    cur.execute("""
+    insert into
+        opensso_services(service_name, username, hashed_password, pre_shared_key)
+        values(%(SERVICE_NAME)s, %(USERNAME)s , %(PASSWORD)s, %(PSK)s)
+    """, {
+        'SERVICE_NAME': name,
+        'USERNAME': username,
+        'PASSWORD': hashlib.sha512(('salt' + password).encode()).hexdigest(),
+        'PSK': PSK
+    })
+    dbcon.commit()
+    return PSK
